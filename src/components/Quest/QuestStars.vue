948bc8bf5c6f436a5e2479147ed96b468621d50b
Shawnkharece <shawnkharece@gmail.com>
2023-09-12 12:25:00 +0800

    SL-11 dynamic stars change

--- src/components/Quest/QuestStars.vue
+++ src/components/Quest/QuestStars.vue
@@ -2,9 +2,16 @@
   <header class="flex justify-center">
     <div
       class="stars-container flex flex-row bg-gradient-to-r from-indigo-900 via-indigo-400 to-indigo-900 border rounded-3xl px-4 py-1">
-      <div v-for="stars in questCards" :key="stars.id">
-        <p>{{ stars.checked }}</p>
-        <StarIcon class="w-5 mx-1" />
+      <div v-for="stars in sortedQuestCards " :key="stars.id">
+        <!-- <p>{{ stars.checked }}</p> -->
+        <!-- <StarIcon class="w-5 mx-1" /> -->
+        <span v-if="stars.checked">
+          <StarIcon class="w-5 mx-1 fill-[#FFE993]" />
+        </span>
+
+        <p v-else>
+          <StarIcon class="w-5 mx-1 fill-gray" />
+        </p>
       </div>
     </div>
   </header>
@@ -23,6 +30,20 @@ export default {
     return {
       questCards,
     };
+  },
+  computed: {
+    sortedQuestCards() {
+      // Create a new array with the sorted elements without modifying the original questCards array
+      return [...this.questCards].sort((a, b) => {
+        if (a.checked && !b.checked) {
+          return -1; // a comes before b
+        } else if (!a.checked && b.checked) {
+          return 1; // b comes before a
+        } else {
+          return 0; // no change in order
+        }
+      });
+    },
   },
 };
 </script>

2e1989f158fa87623037710588d2fe0894cc9674
bryantheball <bryanelijah.18@gmail.com>
2023-10-07 15:14:41 +0800

    SL-15: Added chat tab for message page

---
+++ src/components/Messages/ChatsTab.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div v-if="chatList.length === 0" class="no-chats text-center">
+      <p class="text-white">No chats to display.</p>
+    </div>
+    <div v-else>
+      <div v-for="chat in chatList" :key="chat.id" class="chat-item flex items-center p-2 border-b border-gray-300">
+        <img :src="chat.profilePic" alt="Profile Pic" class="profile-pic w-10 h-10 mr-5" />
+        <div class="chat-details flex-1">
+          <p class="text-gray-400">{{ chat.personId }}</p>
+          <h4 class="text-white font-bold">{{ chat.listingTitle }}</h4>
+          <p class="text-white">{{ chat.latestMessage }}</p>
+        </div>
+        <img :src="chat.listingImage" alt="Listing Image" class="listing-image w-10 h-10" />
+      </div>
+    </div>
+
+    </div>
+
+      
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        chatList: [
+        {
+            id: 1,
+            profilePic: 'src/assets/Icons/Profile.png',
+            personId: 'bryanchua1122',
+            listingTitle: 'Free Onions',
+            listingImage: 'src/assets/Icons/Inventory.png',
+            latestMessage: 'Hello there!',
+        },
+        {
+            id: 2,
+            profilePic: 'src/assets/Icons/Profile.png',
+            personId: 'abigailchua12',
+            listingTitle: 'Dozen of Eggs Available',
+            listingImage: 'src/assets/Icons/Inventory.png',
+            latestMessage: 'How are you?',
+        },
+        {
+            id: 3,
+            profilePic: 'src/assets/Icons/Profile.png',
+            personId: 'johndoe72',
+            listingTitle: 'Freshly harvested vegetables',
+            listingImage: 'src/assets/Icons/Inventory.png',
+            latestMessage: 'Good morning!',
+        },
+          // Add more chat items as needed
+        ],
+      };
+    },
+  };
+  </script>
+  
--- src/views/MessagePage.vue
+++ src/views/MessagePage.vue
@@ -1,11 +1,52 @@
 <template>
-  <div class="container text-white text-center mt-32">
-    <h1>Welcome, Message</h1>
+  <div class="container flex mt-28">
+    <div class="container w-1/3">
+      <div class="tabs tabs-boxed p-2">
+        <a class="tab w-1/2 h-full" :class="{ 'tab-active': currentTab === 'chat' }" @click="changeTab('chat')" style="border-radius:100px">
+          <img src="@/assets/Icons/Chat.png" alt="Custom Icon" class="pr-1 py-2" /> Chat
+        </a>
+        <a class="tab w-1/2 h-full" :class="{ 'tab-active': currentTab === 'notifications' }" @click="changeTab('notifications')" style="border-radius:100px">
+          <img src="@/assets/Icons/ring.png" alt="Custom Icon" class="pr-1 py-2" /> Notifications
+        </a>
+      </div>
+      
+      <div class="content mt-4">
+        <div v-if="currentTab === 'chat'">
+          <ChatsTab />
+        </div>
+        <div v-if="currentTab === 'notifications'">
+          <NotificationsTab />
+        </div>
+      </div>
+    </div>
+    <div class = "desktopchatContainer w-2/3 ml-10">
+      <!-- <p class="text-white">testing</p> -->
+    </div>
+
   </div>
+  
 </template>
 
 <script>
+import ChatsTab from '@/components/Messages/ChatsTab.vue';
+
+
+
 export default {
-  name: 'MessagePage',
+  name:'MessagePage',
+  components: {
+    ChatsTab,
+  },
+  data() {
+    return {
+      currentTab: 'chat',
+
+    };
+  },
+  methods: {
+    changeTab(tabName) {
+      this.currentTab = tabName;
+    },
+  },
 };
 </script>

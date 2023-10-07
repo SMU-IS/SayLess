a27f0d2e8f440654bbb02b82ae08cefc9ef85db3
joshuadavidang <joshuadavidang@outlook.sg>
2023-10-07 16:31:25 +0800

    SL-17: add in stock photos

---
+++ src/components/CardSlider/CardSlider.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="flex flex-row overflow-x-auto gap-6 hide-scroll-bar">
+    <div v-for="item in cardData" :key="item.id">
+      <div
+        class="card md:w-fit card-compact bg-base-100 shadow-xl mt-6 min-h-[250px] bg-white"
+      >
+        <figure>
+          <img :src="item.image" alt="Community Sharing" />
+        </figure>
+        <div class="flex flex-row justify-between items-center card-body">
+          <h3 class="text-md text-black">{{ item.cardTitle }}</h3>
+          <div class="rating">
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-green-500"
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-green-500"
+              checked
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-green-500"
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-green-500"
+            />
+            <input
+              type="radio"
+              name="rating-4"
+              class="mask mask-star-2 bg-green-500"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardSlider',
+  props: {
+    cardData: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>

8c6aea11772c9f207db018e18ba531604476c9b0
joshuadavidang <joshuadavidang@outlook.sg>
2023-10-20 09:29:45 +0800

    SL-40: code refactored

--- src/components/CommunitySharing/CommunitySharing.vue
+++ src/components/CommunitySharing/CommunitySharing.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full">
+  <div>
     <div class="flex justify-between items-baseline">
-      <h4 class="text-white">Community sharing</h4>
+      <h4 class="text-white">Community Sharing</h4>
       <h4 class="text-sm text-green">View All</h4>
     </div>
 
@@ -9,35 +9,27 @@
       <div
         v-for="item in foodList"
         :key="item.id"
-        class="md:max-w-sm w-full max-w-full flex md:block rounded"
+        class="card card-side bg-white text-black shadow-xl rounded-lg cursor-pointer"
         @click="getItemDetails(item.id)"
       >
-        <div
-          class="h-full w-32 md:h-48 md:w-full flex-none bg-cover rounded-l md:rounded-l-none md:rounded-t text-center overflow-hidden"
-          :style="'background-image: url(' + item.listingImages[0] + ')'"
-          title="Woman holding a mug"
-        ></div>
+        <figure>
+          <img :src="item.listingImages[0]" alt="Movie" class="h-full w-32" />
+        </figure>
 
-        <div
-          class="w-full bg-white rounded-r md:rounded-b-none md:rounded-b p-4 flex flex-col justify-between leading-normal"
-        >
-          <div class="mb-4 md:mb-8">
-            <p class="text-sm text-gray-600 flex items-center">Members only</p>
-            <div class="text-gray-900 font-bold text-md mb-2">
-              {{ item.listingTitle }}
+        <div class="flex flex-col justify-center gap-2 p-5">
+          <h2 class="card-title">{{ item.listingTitle }}</h2>
+          <p class="text-black-light text-base">Lorem ipsum dolor sit amet</p>
+
+          <div class="flex items-center gap-3">
+            <div class="avatar">
+              <div class="w-8 rounded-full">
+                <img :src="item.createdBy.profilePic" />
+              </div>
             </div>
-            <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet</p>
-          </div>
-          <div class="flex items-center">
-            <img
-              class="w-7 h-7 md:w-10 md:h-10 rounded-full mr-4"
-              :src="item.listingImages[0]"
-              alt="Avatar of Jonathan Reinink"
-            />
-            <div>
-              <p class="text-gray-900 leading-none text-xs">Jonathan Reinink</p>
-              <p class="text-gray-600 text-xs">Aug 18</p>
-            </div>
+
+            <p class="text-black-light text-xs">
+              {{ item.createdBy.name }}
+            </p>
           </div>
         </div>
       </div>
